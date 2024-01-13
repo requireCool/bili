6e4df414a31d4436fa26216dc80b2cd0ad11102c
Wang <1019715750@qq.com>
2024-01-13 21:16:29 +0800

    change log file pos, add shell log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+
+function log() {
+  echo "[$(date "+%Y-%m-%d %H:%M:%S")] ${1}"
+}
+
 while true; do
+  log "开始任务"
   java -jar /app/BILIBILI-HELPER.jar /config/config.json >> /dev/null
-  if [ -n $TASK_DELAY ]; then
-    echo "下次任务将在${TASK_DELAY}后执行"
+
+  if [ $? -eq 0 ]; then
+    log "任务正常结束"
+  else
+    log "任务异常退出，请检查日志"
+  fi
+
+  if [ -n ${TASK_DELAY} ]; then
+    log "下次任务将在 ${TASK_DELAY} 后执行"
     sleep $TASK_DELAY
   else
-    echo "下次任务将在1d后执行"
+    log "下次任务将在 1d 后执行"
     sleep 1d
   fi
 done
--- src/main/java/top/misec/config/Constant.java
+++ src/main/java/top/misec/config/Constant.java
@@ -4,7 +4,7 @@ package top.misec.config;
  * @author JunzhouLiu
  */
 public class Constant {
-    public static final String LOG_FILE_PATH = "/tmp/bili-helper.log";
+    public static final String LOG_FILE_PATH = "/config/bili-helper.log";
     public static final String BILI_JCT = "bili_jct";
     public static final String BILI_USER_ID = "DedeUserID";
 }
